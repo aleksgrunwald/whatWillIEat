24ea31e4f4f83dca36fcf258fdcd522c8a6d7de6
Aleksandra <aleks.grunwald@gmail.com>
2020-05-30 01:02:46 +0200

    getting random dish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DishesService } from './dishes.service';
+import {Dish} from './dish';
+import {map, take} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'whatWillIEat';
+  clickedButton: boolean = false;
+  waitingForADish: boolean = false;
+
+  // dishes: Dish[];
+  dish: Dish;
+
+  constructor(private dishesService: DishesService) {}
+
+  generateRandomNumber(arrLength) {
+    return Math.floor(Math.random() * arrLength);
+  }
+
+  getRandomDish() {
+    this.clickedButton = true;
+    this.waitingForADish = true;
+    this.dishesService.getDishes()
+      .pipe(
+        map(data => {
+          return data[this.generateRandomNumber(Object.keys(data).length)];
+        })
+      )
+      .subscribe(data => this.dish = data
+      );
+    this.waitingForADish = false;
+  }
+
+  clearDishInfo() {
+    this.clickedButton = false;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatButtonModule} from '@angular/material/button';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatProgressSpinnerModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
