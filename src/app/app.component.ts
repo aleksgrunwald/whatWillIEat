7740b566e8ea430d7ffc9764404ea7bf824ca68a
Aleksandra <aleks.grunwald@gmail.com>
2020-06-05 15:24:05 +0200

    Getting 3 random recipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { DishesService } from './dishes.service';
+import {Component, OnInit} from '@angular/core';
+import {DishesService} from './dishes.service';
 import {Dish} from './dish';
 import {catchError, map, take} from 'rxjs/operators';
 import {of} from 'rxjs';
@@ -10,21 +10,22 @@ import {of} from 'rxjs';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  clickedButton: boolean = false;
+  // clickedButton: boolean = false;
   waitingForADish: boolean = false;
 
-  // dishes: Dish[];
-  dish: Dish;
-  getErrorMsg: string
+  dishes: Dish[] = [];
+  // dish: Dish;
+  randomThreeDishes: Dish[];
+  getErrorMsg: string;
 
-  constructor(private dishesService: DishesService) {}
+  constructor(private dishesService: DishesService) {
+  }
 
   generateRandomNumber(arrLength) {
     return Math.floor(Math.random() * arrLength);
   }
 
   getRandomDish() {
-    this.clickedButton = true;
     this.waitingForADish = true;
     this.dishesService.getDishes()
       .pipe(
@@ -40,13 +41,43 @@ export class AppComponent {
             console.log('EMPTY dataaa');
           }
           console.log('success front');
-          this.dish = data;
+          this.clearDishInfo();
+          this.dishes.push(data);
         },
         error => {
           console.log('err front::  ' + error);
           this.waitingForADish = false;
           this.getErrorMsg = error;
-          },
+        },
+        () => {
+          console.log('complete front');
+          this.waitingForADish = false;
+        }
+      );
+  }
+
+  findThreeDishes() {
+    this.clearDishInfo();
+    this.waitingForADish = true;
+    this.dishesService.getDishes()
+      .pipe(
+        map(data => {
+          const threeRandomDishes: Dish[] = [];
+          for (let i = 0; i < 3; i++) {
+            threeRandomDishes.push(data[this.generateRandomNumber(Object.keys(data).length)]);
+          }
+          return threeRandomDishes;
+        })
+      )
+      .subscribe(
+        data => {
+          this.dishes = data;
+        },
+        error => {
+          console.log('err front::  ' + error);
+          this.waitingForADish = false;
+          this.getErrorMsg = error;
+        },
         () => {
           console.log('complete front');
           this.waitingForADish = false;
@@ -55,11 +86,12 @@ export class AppComponent {
   }
 
   searchRecipeGoogle() {
-    window.open('http://google.com/search?q=' + this.dish.name);
+    // window.open('http://google.com/search?q=' + this.dish.name);
+    window.open('https://www.google.com/search?tbm=isch&q=' + this.dishes[0].name);
   }
 
   clearDishInfo() {
-    this.clickedButton = false;
+    this.dishes = [];
   }
 
 }
